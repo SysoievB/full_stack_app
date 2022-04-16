a9161279d5d253ffb829a348f16f4800f388bef2
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-04-16 08:50:26 +0200

    added 100% coverage for repository

--- src/test/java/com/sysoiev/full_stack_app/repository/StudentRepositoryTest.java
+++ src/test/java/com/sysoiev/full_stack_app/repository/StudentRepositoryTest.java
@@ -2,21 +2,29 @@ package com.sysoiev.full_stack_app.repository;
 
 import com.sysoiev.full_stack_app.model.Gender;
 import com.sysoiev.full_stack_app.model.Student;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository underTestStudentRepository;
 
+    @AfterEach
+    void tearDown() {
+        underTestStudentRepository.deleteAll();
+    }
+
     @Test
     void itShouldCheckWhenStudentEmailExists() {
         // Given
         String email = "ivan@mail.com";
-        Student student = new Student("Ivan", email, Gender.MALE);
+        var student = new Student("Ivan", email, Gender.MALE);
 
         // When
         underTestStudentRepository.save(student);
@@ -24,5 +32,17 @@ class StudentRepositoryTest {
         // Then
         boolean expected = underTestStudentRepository.selectExistsEmail(email);
         assertThat(expected).isTrue();
+    }
+
+    @Test
+    void itShouldCheckWhenStudentEmailDoesNotExist() {
+        // Given
+        String email = "ivan@mail.com";
+
+        // When
+        boolean expected = underTestStudentRepository.selectExistsEmail(email);
+
+        // Then
+        assertThat(expected).isFalse();
     }
 }
---
+++ src/test/java/com/sysoiev/full_stack_app/service/StudentServiceTest.java
@@ -0,0 +1,34 @@
+package com.sysoiev.full_stack_app.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceTest {
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void itShouldGetAll() {
+        // Given
+        // When
+        // Then
+    }
+
+    @Test
+    void itShouldAdd() {
+        // Given
+        // When
+        // Then
+    }
+
+    @Test
+    void itShouldDelete() {
+        // Given
+        // When
+        // Then
+    }
+}
